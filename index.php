5a331043999dee410f9a397a200d741299d32b7f
ezeee21 <105204143+ezeee21@users.noreply.github.com>
2024-03-11 12:11:34 +0100

    feat(view): modification page d'accueil

--- index.php
+++ index.php
@@ -3,9 +3,86 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Accueil</title>
+    <title>ACCUEIL</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;  
+            background-color: #403644;        
+            background-position: center;
+            background-size: cover;
+            background-attachment: fixed;
+         
+        }
+ 
+        nav {
+            background-color: rgb(255,255,255);
+            color: white;
+            padding: 15px;
+        }
+        
+ 
+        nav a {
+            color: white;
+            text-decoration: none;
+            margin: 0 15px;
+            text-align: right;
+            
+        }
+        nav img{
+            width:100px;
+            text-align: center;
+        }
+        .log{
+            margin-top: 13px;
+        }
+ 
+        #inscription, #connexion {
+            display: inline-block;
+            padding: 10px 20px;
+            background-color: #FFB6C1;
+            color: white;
+            text-decoration: none;
+            border-radius: 5px;
+        }
+        #inscription:hover{
+            background-color: #D8BFD8;
+        }
+ 
+        #connexion:hover{
+            background-color: #F0E68C;
+        }
+ 
+        #contenu{
+            display:flex;
+            justify-content: space-between;
+        }
+        #txt{
+            color:white;
+            font-family:Impact,fantasy;
+            font-weight: bold;
+            display:flex;
+            justify-content: center;
+        }
+    </style>
 </head>
 <body>
-    
+ 
+    <nav> <!-- Balise de navigation -->
+        <div id="contenu"> <!-- Conteneur de la barre de navigation -->
+            <div> <!-- Première partie de la barre de navigation : logo -->
+                <img class="logo" src="assets/quizzeo.png"> <!-- Logo -->
+            </div>
+            <div class='log'> <!-- Deuxième partie de la barre de navigation : liens d'inscription et de connexion -->
+                <a href="./inscription.php" id="inscription">Inscription</a> <!-- Lien vers la page d'inscription -->
+                <a href="./login.php" id="connexion">Connexion</a> <!-- Lien vers la page de connexion -->
+            </div>
+        </div>
+    </nav>
+ 
+   <h1 id="txt">Créez ou participez aux meilleurs quizz</h1>
+ 
+ 
 </body>
 </html>
