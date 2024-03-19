c593114e37e9eea3caa3b4a5b013938b7356442c
L'ADMINISTRATEUR S <s.simonis@ecole-ipssi.net>
2024-03-19 13:04:07 +0100

    feat:quizz

--- creationquizz.php
+++ creationquizz.php
@@ -15,38 +15,39 @@ if (isset($_POST['nom']) && isset($_POST['action'])&& isset($_GET['id'])) { // V
     
     if ($_POST['action'] == 'Ajouter') { // Vérifie si l'action est d'ajouter une attraction aux favoris
         $file = fopen('quizz.csv', 'a+'); // Ouvre à nouveau le fichier des favoris en mode lecture et écriture
-        if ($_FILES['image']['error'] === UPLOAD_ERR_OK) {
+            if ($_FILES['image']['error'] === UPLOAD_ERR_OK) {
             // Spécifier le dossier de destination où stocker le fichier téléchargé
-            $uploadDirectory = 'C:\xampp\htdocs\quizzeo\Asset/';
-            $chemin="Asset/";
-            // Récupérer le nom du fichier téléchargé
+             $uploadDirectory = 'C:\xampp\htdocs\quizzeo\Asset/';
+             $chemin="Asset/";
+             // Récupérer le nom du fichier téléchargé
             $fileName = $_FILES['image']['name'];
         
             // Construire le chemin complet du fichier de destination
-            $destination = $uploadDirectory . $fileName;
-            $affiche = $chemin .$fileName;
-        
+              $destination = $uploadDirectory . $fileName;
+             $affiche = $chemin .$fileName;
+                
             // Déplacer le fichier téléchargé vers le dossier de destination
-            if (move_uploaded_file($_FILES['image']['tmp_name'], $destination)) {
+             if (move_uploaded_file($_FILES['image']['tmp_name'], $destination)) {
                 echo 'Le fichier a été téléchargé avec succès.';
-            } 
-        }
-                if($_SESSION['ajouts']<6){
+             } 
+         }
+        
+         if($_SESSION['ajouts']<6){
                     fputcsv($file,[$_POST['nom'],$_GET['id'],$_POST['qst1'],$_POST['reponse1'],$_POST['reponse2'],$_POST['reponse3'],$_POST['reponse4'],$_POST['point'], $affiche]);
                     $_SESSION['ajouts']++; // Incrémenter le compteur
                     header('location: ./pagecreation.php');
 
-                }
+                    }
             
-        // Logique pour vérifier et ajouter l'attraction aux favoris
-            fclose($file); // Ferme le fichier des favoris
+                // Logique pour vérifier et ajouter l'attraction aux favoris
+                    fclose($file); // Ferme le fichier des favoris
            
    
-            }
-            elseif ($_POST['action'] == 'Enregistre') { // Vérifie si l'action est d'ajouter une attraction aux favoris
-            
-                $_SESSION['ajouts']=6;
-                header('location: ./pagecreation.php');
+          }
 
-                  }
+}
+elseif ($_POST['action'] == 'Enregistre') { // Vérifie si l'action est d'ajouter une attraction aux favoris
+    $_SESSION['ajouts']=6;
+    header('location: ./pagecreation.php');
+
 }
